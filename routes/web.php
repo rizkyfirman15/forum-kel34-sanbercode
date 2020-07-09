f449fd8bb1dd6aa0f8e3f97be9480ded5453c469
iqbalShafiq <iqbal.syafiq.rozaan@gmail.com>
2020-07-09 22:44:55 +0700

    Menambahkan fitur untuk answer

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,4 @@
 <?php
-
-use App\Http\Controllers\AnswerController;
-use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\Auth;
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,13 +11,14 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/question/create', 'QuestionController@create');
     Route::post('/question', 'QuestionController@store');
     Route::get('/question/1', 'QuestionController@show');
-    // Answer Controller
-    Route::resource('/answer', 'AnswerController');
-    Route::put('/answer/{id}/vote', 'AnswerController@vote');
-    Route::get('/answer/{id}/create', 'AnswerController@create');
-    Route::post('/answer/{id}/create', 'AnswerController@create');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Answer Controller
+Route::resource('/answer', 'AnswerController');
+Route::put('/answer/{id}/vote', 'AnswerController@vote');
+Route::get('/answer/{id}/create', 'AnswerController@create');
+Route::post('/answer/{id}/create', 'AnswerController@create');
